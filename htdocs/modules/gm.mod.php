a6caa7a3fd6992d553feaf54bf53475c30051787
Takehiko Ono <me@onotakehiko.com>
2023-09-14 22:11:04 +0900

    sp | 言語バーの並びを変更

--- htdocs/modules/gm.mod.php
+++ htdocs/modules/gm.mod.php
@@ -68,9 +68,9 @@
 						<li><a href="#" target="_blank" rel="noopener">サイトポリシー</a></li>
 					</ul>
 					<ul class="p-gm__footer__languages" data-stt-langbar>
-						<li><a href="#" data-stt-changelang="zh-CN" data-stt-ignore><span>中文（<span>简</span>体）</span></a></li>
-						<li><a href="#" lang="en" data-stt-changelang="en" data-stt-ignore><span>English</span></a></li>
-						<li><a href="#" data-stt-changelang="ja" data-stt-ignore data-stt-active><span>日本語</span></a></li>
+						<li class="is--cn"><a href="#" data-stt-changelang="zh-CN" data-stt-ignore><span>中文（<span>简</span>体）</span></a></li>
+						<li class="is--en"><a href="#" lang="en" data-stt-changelang="en" data-stt-ignore><span>English</span></a></li>
+						<li class="is--ja"><a href="#" data-stt-changelang="ja" data-stt-ignore data-stt-active><span>日本語</span></a></li>
 					</ul>
 					<p class="p-gm__footer__copyright" lang="en">©︎ KUME SEKKEI co.ltd.</p>
 				</div>
